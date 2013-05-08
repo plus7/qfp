d1210f1475282b3bc2b41dc7c098296e1ea3b4f7
NOSE, Takafumi <ahya365@gmail.com>
2013-05-08 22:16:26 +0900

    256通り列挙するプログラムを追加

---
+++ listall.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* 手抜き */
+void print_qfp(unsigned char val)
+{
+  unsigned char sign = val >> 7;
+  unsigned char exponent = (val & 0x70) >> 4;
+  unsigned char fraction = val & 0x0f;
+
+  if(sign) printf("-");
+  if(exponent){
+    printf("%f\n", pow(2, exponent - 3) * (1.0                               +
+					   0.5    * ((fraction & 0x08) >> 3) + 
+					   0.25   * ((fraction & 0x04) >> 2) +
+					   0.125  * ((fraction & 0x02) >> 1) +
+					   0.0625 * (fraction & 0x01)));
+  }else{ /* Denormal number */
+    printf("%f\n", pow(2, -2) * (0.5    * ((fraction & 0x08) >> 3) + 
+				 0.25   * ((fraction & 0x04) >> 2) +
+				 0.125  * ((fraction & 0x02) >> 1) +
+				 0.0625 * (fraction & 0x01)));
+  }
+}
+
+int main(void)
+{
+  int i;
+  for(i = 0; i < 256; i++){
+    printf("%02x = ", i);
+    print_qfp(i);
+  }
+  return 0;
+}
